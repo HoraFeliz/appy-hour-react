7112679297a199e674d81a4eae1be024b1951b46
Jaime Baro <jaimebaroval@gmail.com>
2020-10-24 10:38:44 +0200

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,10 +6,7 @@ import { faSearchLocation, faBars } from '@fortawesome/free-solid-svg-icons'
 function Navbar() {
     return (
         <nav className="appy--navbar">
-            <Link className="appy--navbar-brand" to="/tours"><FontAwesomeIcon icon={faBars} /></Link>
-            {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button> */}
+            <Link className="appy--navbar-brand"><FontAwesomeIcon icon={faBars} /></Link>
 
             <form className="appy--search-form">
                 <input className="appy--search-input" type="search" placeholder="Search Tours" aria-label="Search" />
