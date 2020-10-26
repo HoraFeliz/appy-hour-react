3be6e719e9408dc8a947b20b6abfc1ffbe0ae5c6
Irma Blanco <irblancofdz@gmail.com>
2020-10-26 20:12:03 +0100

    task: Swipe tours

--- src/components/tours/TourItem.jsx
+++ src/components/tours/TourItem.jsx
@@ -1,47 +1,61 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapMarkerAlt, faWalking, faRoute } from '@fortawesome/free-solid-svg-icons'
-import TourImageCanvas from '../common/ImageCanvas';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faWalking,
+  faRoute,
+} from "@fortawesome/free-solid-svg-icons";
+import TourImageCanvas from "../common/ImageCanvas";
 
 class TourItem extends Component {
-
-    render() {
-
-        return (
-            <div className="appy--tours-item">
-                <TourImageCanvas recommended={this.props.recommended} brand={this.props.brand} place={this.props.place} />
-                <div className={(this.props.brand === `recommended`) ?
-                    `appy--tours-item-distancebar recommended` : `appy--tours-item-distancebar`}>
-                    <div className="appy--tours-item-distancebar-distante-tour">
-                        <div className="appy--tours-item-distancebar-icon">
-                            <FontAwesomeIcon icon={faWalking} />
-                        </div>
-                        <div className="appy--tours-item-distancebar-text">
-                            12 Km.
-                            </div>
-                    </div>
-                    <div className="appy--tours-item-distancebar-distante-nearby">
-                        <div className="appy--tours-item-distancebar-icon">
-                            <FontAwesomeIcon icon={faMapMarkerAlt} />
-                        </div>
-                        <div className="appy--tours-item-distancebar-text">
-                            300 m.
-                            </div>
-                    </div>
-                </div>
-
-                <div className="appy--tours-item-info">
-                    <h3 className="appy--tours-item-info-title">Mahou Historical Tour</h3>
-                    <p className="appy--tours-item-info-description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo quibusdam perspiciatis.</p>
-                    <div className="appy--tours-item-info-creator">
-                        <div className="appy--tours-item-info-creator-icon"><FontAwesomeIcon icon={faRoute} /></div>
-                        <div className="appy--tours-item-info-creator-text">Appy Hour Tours</div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="appy--tours-item">
+        <TourImageCanvas
+          recommended={this.props.recommended}
+          brand={this.props.brand}
+          place={this.props.place}
+        />
+        <div
+          className={
+            this.props.brand === `recommended`
+              ? `appy--tours-item-distancebar recommended`
+              : `appy--tours-item-distancebar`
+          }
+        >
+          <div className="appy--tours-item-distancebar-distante-tour">
+            <div className="appy--tours-item-distancebar-icon">
+              <FontAwesomeIcon icon={faWalking} />
             </div>
-        );
-    }
+            <div className="appy--tours-item-distancebar-text">12 Km.</div>
+          </div>
+          <div className="appy--tours-item-distancebar-distante-nearby">
+            <div className="appy--tours-item-distancebar-icon">
+              <FontAwesomeIcon icon={faMapMarkerAlt} />
+            </div>
+            <div className="appy--tours-item-distancebar-text">300 m.</div>
+          </div>
+        </div>
+
+        <div className="appy--tours-item-info">
+          <h3 className="appy--tours-item-info-title">
+            {this.props.tour ? this.props.tour.name : "Loading"}
+          </h3>
+          <p className="appy--tours-item-info-description">
+            {this.props.tour ? this.props.tour.description : "Loading"}
+          </p>
+          <div className="appy--tours-item-info-creator">
+            <div className="appy--tours-item-info-creator-icon">
+              <FontAwesomeIcon icon={faRoute} />
+            </div>
+            <div className="appy--tours-item-info-creator-text">
+              Appy Hour Tours
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TourItem;
-
--- src/components/tours/Tours.jsx
+++ src/components/tours/Tours.jsx
@@ -5,71 +5,72 @@ import ReactSwipe from "react-swipe";
 import Button from "../common/Button";
 import { getTours } from "../../services/api-client";
 
-
 class Tours extends React.Component {
-    // let reactSwipeEl;
+  // let reactSwipeEl;
 
-    state = {
-        tours: [],
-    }
+  state = {
+    tours: [],
+  };
 
-    fetchTours = () => {
-        getTours().then((tours) => {
-            this.setState({ tours })
-        })
-    }
+  fetchTours = () => {
+    getTours().then((tours) => {
+      this.setState({ tours });
+    });
+  };
 
-    componentDidMount() {
-        this.fetchTours()
-    }
+  componentDidMount() {
+    this.fetchTours();
+  }
 
-    render() {
-        return (
-            <div>
-                <TourBarInfo recommended={true}>
-                    <Button num="5" info={true} recommended={true} />
-                    <Button type="map" info={true} recommended={true} />
-                </TourBarInfo>
-                <ReactSwipe props={this.state.tours} className="carousel" swipeOptions={{ continuous: false }}>
-                    <div style={{ display: "inline-flex" }}>
-                        <TourItem recommended={true} brand="mahou" />
-                        <TourItem recommended={true} brand="recommended" />
-                    </div>
-                    <div style={{ display: "inline-flex" }}>
-                        <TourItem recommended={true} brand="recommended" />
-                        <TourItem recommended={true} brand="recommended" />
-                    </div>
-                    <div style={{ display: "inline-flex" }}>
-                        <TourItem recommended={true} brand="recommended" />
-                        <TourItem recommended={true} brand="recommended" />
-                    </div>
-                </ReactSwipe>
+  render() {
+    return (
+      <div>
+        <TourBarInfo recommended={true}>
+          <Button num="5" info={true} recommended={true} />
+          <Button type="map" info={true} recommended={true} />
+        </TourBarInfo>
+        <ReactSwipe className="carousel" swipeOptions={{ continuous: false }}>
+          <div style={{ display: "inline-flex" }}>
+            {this.state.tours.length ? (
+              this.state.tours.map((tour, key) => (
+                <TourItem
+                  key={key}
+                  tour={tour}
+                  recommended={true}
+                  brand="mahou"
+                />
+              ))
+            ) : (
+              <p className="text-center">Loading...</p>
+            )}
+          </div>
+        </ReactSwipe>
 
-                <TourBarInfo>
-                    <Button num="5" info={true} />
-                    <Button type="map" info={true} />
-                </TourBarInfo>
-                <ReactSwipe
-                    className="carousel"
-                    swipeOptions={{ continuous: false }}
-                    widthOfSiblingSlidePreview="50px"
-                >
-                    <div style={{ display: "inline-flex" }}>
-                        <TourItem />
-                        <TourItem />
-                    </div>
-                    <div style={{ display: "inline-flex" }}>
-                        <TourItem />
-                        <TourItem />
-                    </div>
-                    <div style={{ display: "inline-flex" }}>
-                        <TourItem />
-                        <TourItem />
-                    </div>
-                </ReactSwipe>
-            </div>
-        );
-    }
+        <TourBarInfo>
+          <Button num="5" info={true} />
+          <Button type="map" info={true} />
+        </TourBarInfo>
+        <ReactSwipe
+          className="carousel"
+          swipeOptions={{ continuous: false }}
+          widthOfSiblingSlidePreview="50px"
+        >
+          <div style={{ display: "inline-flex" }}>
+            <TourItem />
+            <TourItem />
+          </div>
+          <div style={{ display: "inline-flex" }}>
+            <TourItem />
+            <TourItem />
+          </div>
+          <div style={{ display: "inline-flex" }}>
+            <TourItem />
+            <TourItem />
+          </div>
+        </ReactSwipe>
+      </div>
+    );
+  }
 }
 
-export default Tours; 
+export default Tours;
