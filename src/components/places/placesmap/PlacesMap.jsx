911c4266e178b1d6d25d40b24ad4b805d8bbe239
Irma Blanco <irblancofdz@gmail.com>
2020-11-02 11:47:19 +0100

    wip: Map

--- src/components/places/placesmap/PlacesMap.jsx
+++ src/components/places/placesmap/PlacesMap.jsx
@@ -1,31 +1,27 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import L, { map } from "leaflet";
-import "./PlacesMap.scss";
+import L from "leaflet";
+import { Map, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import "./PlacesMap.scss";
 
-// import locations from "../../../assets/data/locations.json";
+import utensilsIcon from "../../../assets/images/utensils-marker.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 import { getLocations } from "../../../services/api-client";
 
+import locations from "../../../assets/data/locations.json";
+const MAPBOX_API_KEY = process.env.REACT_APP_MAPBOX_API_KEY;
+const MAPBOX_USERID = process.env.REACT_APP_MAPBOX_USERID;
+const MAPBOX_STYLEID = process.env.REACT_APP_MAPBOX_STYLEID;
+
 const PlacesMap = () => {
-  const [locations, setLocations] = useState([]);
+  // const [locations, setLocations] = useState([]);
 
-  useEffect(() => {
-    getLocations().then((locations) => {
-      setLocations(locations);
-    });
-  }, []);
-
+  // useEffect(() => {
+  //   getLocations().then((locations) => {
+  //     setLocations(locations);
+  //   });
+  // }, []);
   const mapRef = useRef();
-
-  useEffect(() => {
-    delete L.Icon.Default.prototype._getIconUrl;
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-      iconUrl: require("leaflet/dist/images/marker-icon.png"),
-      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-    });
-  }, []);
 
   useEffect(() => {
     delete L.Icon.Default.prototype._getIconUrl;
@@ -53,15 +49,20 @@ const PlacesMap = () => {
     });
 
     const geoJson = new L.GeoJSON(locations, {
+      pointToLayer: (feature, latlng) => {
+        return L.marker(latlng, {
+          icon: new L.Icon({
+            iconUrl: utensilsIcon,
+            iconSize: [26, 26],
+            popupAnchor: [0, -15],
+            shadowUrl: markerShadow,
+            shadowAnchor: [13, 28],
+          }),
+        });
+      },
       onEachFeature: (feature = {}, layer) => {
-        /**
-         * @lesson-08-todo
-         * Being able to access each feature gives us the ability to
-         * access all of the data that we've created in our GeoJSON
-         * file. How can we use it to show delivery zones on the map?
-         */
-
         const { properties = {} } = feature;
+        console.log("properties", properties);
         const { name, delivery, tags, phone, website } = properties;
 
         const popup = L.popup();
@@ -93,22 +94,24 @@ const PlacesMap = () => {
     });
 
     geoJson.addTo(map);
-  }, [locations, mapRef]);
+  }, [mapRef]);
+
+  useEffect(() => {
+    const { current = {} } = mapRef;
+    const { leafletElement: map } = current;
+
+    if (!map) return;
+  }, [mapRef]);
 
   return (
-    <Map ref={mapRef} center={[38.907132, -77.036546]} zoom={12}>
-      {/**
-       * @lesson-03-todo
-       * Now that we created a new Map Style with Mapbox,
-       * we want to use it in our app so that we can have
-       * a custom map. To do that, we'll need to update our
-       * TileLayer to use a Mapbox endpoint and attribution.
-       */}
-      <TileLayer
-        url={`https://api.mapbox.com/styles/v1/${process.env.REACT_APP_MAPBOX_USERID}/${process.env.REACT_APP_MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_API_KEY}`}
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-      />
-    </Map>
+    <React.Fragment>
+      <Map ref={mapRef} center={[38.907132, -77.036546]} zoom={12}>
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+        />
+      </Map>
+    </React.Fragment>
   );
 };
 
