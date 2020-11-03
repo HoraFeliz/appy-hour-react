b34f00bd2bb9ab53a6f08a2c4f02c72b10eee3c4
Irma Blanco <irblancofdz@gmail.com>
2020-11-03 17:47:52 +0100

    feat: Recommended tour

--- src/components/forms/form/Form.jsx
+++ src/components/forms/form/Form.jsx
@@ -1,104 +1,83 @@
 import React from "react";
 import { createTour } from "../../../services/api-client";
 import { withRouter } from "react-router-dom";
-import TextInput from "../form-components/text-input/TextInput";
-import TextArea from "../form-components/textarea/TextArea";
-const validations = {
-  name: (value) => value.length > 1,
-  description: (value) => value.length > 1,
-};
 
 class Form extends React.Component {
-  state = {
-    data: {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recommended: false,
       name: "",
       description: "",
-    },
-    error: {
-      name: true,
-      description: true,
-    },
-    touch: {},
-  };
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    createTour(this.state.data)
+    createTour(this.state)
       .then((tour) => {
         this.props.history.push(`/tour/add/${tour.id}`);
       })
       .catch((err) => console.log("Error creating tour", err));
   };
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-
-    const validationFn = validations[name];
-    const isValid = validationFn(value);
-
-    this.setState({
-      data: {
-        ...this.state.data,
-        [name]: value,
-      },
-      error: {
-        ...this.state.error,
-        [name]: !isValid,
-      },
-    });
-  };
-
-  handleBlur = (event) => {
-    const { name } = event.target;
-
-    this.setState({
-      touch: {
-        ...this.state.touch,
-        [name]: true,
-      },
-    });
-  };
-
   render() {
-    const { data, error, touch } = this.state;
-
-    const isError = Object.values(error).some((el) => el === true);
-
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <form onSubmit={this.handleSubmit}>
-              <TextInput
-                name="name"
-                value={data.name}
-                onBlur={this.handleBlur}
-                onChange={this.handleChange}
-                error={error.name}
-                touch={touch.name}
-              />
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            required
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          description
+          <input
+            required
+            name="description"
+            type="text"
+            value={this.state.description}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
 
-              <TextArea
-                name="description"
-                value={data.description}
-                onBlur={this.handleBlur}
-                onChange={this.handleChange}
-                error={error.description}
-                touch={touch.description}
-              />
+        <label>
+          Is Recommended
+          <input
+            name="recommended"
+            type="checkbox"
+            checked={this.state.reocommended}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
 
-              <button
-                onClick={this.handleTourCreated}
-                type="submit"
-                className="btn btn-primary"
-                disabled={isError}
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
+        <button
+          onClick={this.handleTourCreated}
+          type="submit"
+          className="btn btn-primary"
+        >
+          Submit
+        </button>
+      </form>
     );
   }
 }
