dabf613c9d04ba8ddb3256b279aac5751a2e9c96
Jaime Baro <jaimebaroval@gmail.com>
2021-02-23 11:37:13 +0100

    squeleton tourdetail

--- src/components/tours/TourDetail.jsx
+++ src/components/tours/TourDetail.jsx
@@ -3,12 +3,13 @@ import InfoBar from '../infobar/InfoBar';
 import MapWithADirectionsRenderer from './directionsrenderer/DirectionsRenderer';
 import PlaceListItem from '../places/PlaceListItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt, faRoute, faStopwatch, faWalking } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt, faPhotoVideo, faRoute, faStopwatch, faWalking } from '@fortawesome/free-solid-svg-icons';
 import BeerRating from '../common/BeerRating';
 import { getPlaces, getTourById } from '../../services/api-client';
 import AppyButton from '../common/AppyButton';
 import { useHistory } from 'react-router-dom';
 import { FacebookShareButton, TwitterShareButton, WhatsappShareButton } from 'react-share';
+import PlaceInfo from '../places/PlaceInfo';
 
 class TourDetail extends Component {
 	state = {
@@ -23,19 +24,19 @@ class TourDetail extends Component {
 	componentDidMount() {
 		this.fetchPlaces();
 		this.fetchTour();
-		
+
 	}
 
 	calculateDistance = (...places) => {
 		if (Object.keys(this.state.tour).length !== 0) {
 			const service = new window.google.maps.DistanceMatrixService();
 
-			const addresOrigin = [ ...places ];
-			const origin = [ addresOrigin[0].address ];
+			const addresOrigin = [...places];
+			const origin = [addresOrigin[0].address];
 			// const destinations = [addresOrigin.address];
-			const origin2 = [ addresOrigin[1].address ];
+			const origin2 = [addresOrigin[1].address];
 			// const destinations2 = [addresOrigin[2].address];
-			const restOfPlaces = [ ...places ];
+			const restOfPlaces = [...places];
 
 			let destinations = [];
 			restOfPlaces.forEach((place) => {
@@ -64,7 +65,7 @@ class TourDetail extends Component {
 						const originList = response.originAddresses;
 						for (let i = 0; i < originList.length; i++) {
 							const results = response.rows[i].elements;
-							this.setState({ directions: [ ...results ] });
+							this.setState({ directions: [...results] });
 
 							const directions = this.state.directions;
 
@@ -109,7 +110,7 @@ class TourDetail extends Component {
 
 	ratingAppyHour = (...places) => {
 		let totalRatingSum = 0;
-		const placesArray = [ ...places ];
+		const placesArray = [...places];
 
 		placesArray && placesArray.map((place) => (totalRatingSum += place.rating));
 
@@ -119,111 +120,135 @@ class TourDetail extends Component {
 
 	render() {
 		const { id } = this.state.tour;
-		const hashtag = [ 'appyhour', 'beer', 'enjoywithfriend' ];
+		const hashtag = ['appyhour', 'beer', 'enjoywithfriend'];
 		const title = 'Appy Hour Tours';
 		const url = `${process.env.REACT_APP_URL}/tour/${id}`;
-	
+
 		return (
-			<div>
-				<InfoBar back={true} tour={this.state.tour} place={this.state.places} />
-				<div className="appy--tours-detail">
-					<MapWithADirectionsRenderer />
-				</div>
+			<>
+				{this.state.places.length ?
 
-				<div className="appy--tours-detail-info">
-					<h2 className="appy--tours-detail-info-placename">{this.state.tour.name}</h2>
-					<div className="appy--tours-item-info-creator">
-						<div className="appy--tours-item-info-creator-icon">
-							<FontAwesomeIcon icon={faRoute} />
+					<div>
+						<InfoBar back={true} tour={this.state.tour} place={this.state.places} />
+						<div className="appy--tours-detail">
+							<MapWithADirectionsRenderer />
 						</div>
-						<div className="appy--tours-item-info-creator-text">Appy Hour Tours</div>
-					</div>
-					<p className="appy--tours-detail-info-description">{this.state.tour.description}</p>
-					<hr />
-					<div className={`appy--tours-detail-distancebar`} style={{ backgroundColor: 'white' }}>
-						<div className="appy--tours-item-distancebar-distante-tour">
-							<div className="appy--tours-item-distancebar-icon">
-								<FontAwesomeIcon icon={faWalking} />
-							</div>
-							<div className="appy--tours-item-distancebar-text" style={{ color: '#707070' }}>
-								{(this.state.totalDistance / 1000).toFixed(2)} Km
-							</div>
-						</div>
-						<div className="appy--tours-item-distancebar-distante-tour">
-							<div className="appy--tours-item-distancebar-icon">
-								<FontAwesomeIcon icon={faStopwatch} />
-							</div>
-							<div className="appy--tours-item-distancebar-text" style={{ color: '#707070' }}>
-								{(this.state.totalDuration / 60).toFixed(0)} min
-							</div>
-						</div>
-						<div className="appy--tours-item-distancebar-distante-nearby">
-							<div className="appy--tours-item-distancebar-icon">
-								<FontAwesomeIcon icon={faMapMarkerAlt} />
-							</div>
-							<div className="appy--tours-item-distancebar-text" style={{ color: '#707070' }}>
-								{this.state.places.length + ' Places'}
-							</div>
-						</div>
-					</div>
-					<hr />
-					{this.state.places.length ? (
-						this.state.places.map((place, key) => (
-							<PlaceListItem
-								key={key}
-								type="num"
-								num={key}
-								total={this.state.places.length}
-								recommended={false}
-								place={place}
-								directions={this.state.directions[key]}
-								tour={this.state.tour}
-							/>
-						))
-					) : (
-						'NO PLACES'
-					)}
-					<hr style={{ marginBottom: '10px' }} />
-					<div className="appy--tours-detail-rating-container">
-						<div className="appy--row">
-							<div className="appy--col-6">
-								<span className="appy--tours-detail-rating-text">Appy Hour Rating</span>
-							</div>
-							<div className="appy--col-6">
-								<BeerRating type="tour-detail" rating={this.state.totalRating} />
-							</div>
-						</div>
-					</div>
-					<hr style={{ marginTop: '10px' }} />
-					<div className="appy--tours-detail-share">
-						<div className="container">
-							<div className="row">
-								<div className="appy--col-6">
-									<span className="appy--tours-detail-share-text">Share Tour</span>
+
+						<div className="appy--tours-detail-info">
+							<h2 className="appy--tours-detail-info-placename">{this.state.tour.name}</h2>
+							<div className="appy--tours-item-info-creator">
+								<div className="appy--tours-item-info-creator-icon">
+									<FontAwesomeIcon icon={faRoute} />
 								</div>
-								<div className="appy--col-6">
-									<div className="appy--tours-detail-share-buttons">
-										<WhatsappShareButton url={url} title={title}>
-											<AppyButton num="info" type="whatsapp" />
-										</WhatsappShareButton>
+								<div className="appy--tours-item-info-creator-text">Appy Hour Tours</div>
+							</div>
+							<p className="appy--tours-detail-info-description">{this.state.tour.description}</p>
+							<hr />
+							<div className={`appy--tours-detail-distancebar`} style={{ backgroundColor: 'white' }}>
+								<div className="appy--tours-item-distancebar-distante-tour">
+									<div className="appy--tours-item-distancebar-icon">
+										<FontAwesomeIcon icon={faWalking} />
+									</div>
+									<div className="appy--tours-item-distancebar-text" style={{ color: '#707070' }}>
+										{(this.state.totalDistance / 1000).toFixed(2)} Km
+									</div>
+								</div>
+								<div className="appy--tours-item-distancebar-distante-tour">
+									<div className="appy--tours-item-distancebar-icon">
+										<FontAwesomeIcon icon={faStopwatch} />
+									</div>
+									<div className="appy--tours-item-distancebar-text" style={{ color: '#707070' }}>
+										{(this.state.totalDuration / 60).toFixed(0)} min
+									</div>
+								</div>
+								<div className="appy--tours-item-distancebar-distante-nearby">
+									<div className="appy--tours-item-distancebar-icon">
+										<FontAwesomeIcon icon={faMapMarkerAlt} />
+									</div>
+									<div className="appy--tours-item-distancebar-text" style={{ color: '#707070' }}>
+										{this.state.places.length + ' Places'}
+									</div>
+								</div>
+							</div>
+							<hr />
+							{this.state.places.length ? (
+								this.state.places.map((place, key) => (
+									<PlaceListItem
+										key={key}
+										type="num"
+										num={key}
+										total={this.state.places.length}
+										recommended={false}
+										place={place}
+										directions={this.state.directions[key]}
+										tour={this.state.tour}
+									/>
+								))
+							) : (
+									'NO PLACES'
+								)}
+							<hr style={{ marginBottom: '10px' }} />
+							<div className="appy--tours-detail-rating-container">
+								<div className="appy--row">
+									<div className="appy--col-6">
+										<span className="appy--tours-detail-rating-text">Appy Hour Rating</span>
+									</div>
+									<div className="appy--col-6">
+										<BeerRating type="tour-detail" rating={this.state.totalRating} />
+									</div>
+								</div>
+							</div>
+							<hr style={{ marginTop: '10px' }} />
+							<div className="appy--tours-detail-share">
+								<div className="container">
+									<div className="row">
+										<div className="appy--col-6">
+											<span className="appy--tours-detail-share-text">Share Tour</span>
+										</div>
+										<div className="appy--col-6">
+											<div className="appy--tours-detail-share-buttons">
+												<WhatsappShareButton url={url} title={title}>
+													<AppyButton num="info" type="whatsapp" />
+												</WhatsappShareButton>
 
-										<FacebookShareButton url={url} hashtag="#AppyHour">
-											<AppyButton num="info" type="facebook" />
-										</FacebookShareButton>
-										<TwitterShareButton
-											url={url}
-											title={title}
-											hashtags={hashtag}
-										>
-											<AppyButton num="info" type="twitter" />
-										</TwitterShareButton>
+												<FacebookShareButton url={url} hashtag="#AppyHour">
+													<AppyButton num="info" type="facebook" />
+												</FacebookShareButton>
+												<TwitterShareButton
+													url={url}
+													title={title}
+													hashtags={hashtag}
+												>
+													<AppyButton num="info" type="twitter" />
+												</TwitterShareButton>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+
+					:
+
+					<>
+						<div className="appy--tours-detail loading--background-default loading--background-default-touch icon-map" style={{ height: '300px' }}>
+							<div className="appy--place-item-map-canvas mb-3 ">
+								<FontAwesomeIcon icon={faRoute}></FontAwesomeIcon>
+							</div>
+							<InfoBar loading={true} />
+
+						</div>
+						<div style={{ padding: '0px 5px' }}>
+							<PlaceListItem loading={true} />
+							<PlaceListItem loading={true} />
+							<PlaceListItem loading={true} />
+						</div>
+					</>
+
+				}
+
+			</>
 		);
 	}
 }
